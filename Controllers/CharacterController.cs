ccc0f5546ad0889edd4efd176a962d60d5c7f34b
Joshua Shackelford <noescapes08@macbook-pro-021.lan>
2020-08-22 17:30:01 -0500

    Fixing to Start DTOs

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,17 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CharacterController: ControllerBase
+    public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private readonly ICharacterService _characterService;
 
-        public IActionResult Get()
+        public CharacterController(ICharacterService characterService)
         {
-            return Ok(knight);
+            _characterService = characterService;
+
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterByID(id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
